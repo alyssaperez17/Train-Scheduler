4f718eade8babe6c5f6bc98514cd6c67e31cb8a1
Alyssa Perez <aperez@censeohealth.com>
2019-07-25 09:24:16 -0500

    wrote out plan

---
+++ assets/javascript/train.js
@@ -0,0 +1,6 @@
+// create database
+//create on click to add train info
+// grab value from user input - send to db
+// do child added function
+// use moment js to convert time 
+// should display on page
